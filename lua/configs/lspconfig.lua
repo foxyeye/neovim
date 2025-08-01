9db6e5010c5b88e64ab6c5fe5d6c64830e962927
foxyeye <icefoxfly@gmail.com>
2025-08-01 19:47:37 +0800

    add go lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,3 +4,14 @@ local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+
+-- lspconfig 配置 gopls
+require("lspconfig").gopls.setup({
+  cmd = { "gopls" },  -- 使用环境变量中的 gopls
+  settings = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+    },
+  },
+})
