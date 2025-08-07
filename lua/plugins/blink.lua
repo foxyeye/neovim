461bd68e2bb93cea7881f7346b35397d5af304c3
foxyeye <icefoxfly@gmail.com>
2025-08-07 14:50:02 +0800

    change blink keymap

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+      },
+    },
+  },
+}
