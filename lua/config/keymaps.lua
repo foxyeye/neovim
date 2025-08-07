84b96e06be50583f850d91de3e5020293ab3d8e3
foxyeye <icefoxfly@gmail.com>
2025-08-07 15:57:55 +0800

    add Ctrl+f/b keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 插入模式下 ctrl-f 和 ctrl-b
+vim.keymap.set("i", "<C-f>", "<Right>", { desc = "Move right in insert mode" })
+vim.keymap.set("i", "<C-b>", "<Left>", { desc = "Move left in insert mode" })
